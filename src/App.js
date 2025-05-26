0af1f61fc243c1c9bdbed10931dccef5b5de4cd1
jzerra <jzerra01@gmail.com>
2025-05-26 22:46:25 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: 20, fontFamily: 'Arial, sans-serif' }}>
+      <h1>Welcome to Token Mini-App</h1>
+      {/* We will build the UI here */}
+    </div>
+  );
+}
